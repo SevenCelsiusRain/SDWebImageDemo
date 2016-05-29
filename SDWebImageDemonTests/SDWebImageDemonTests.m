2969031ee7a016f55ca0e0ae5c84b29eeba653b5
七度雨 <269656131@qq.com>
2016-05-29 09:36:18 +0800

    Initial Commit

---
+++ SDWebImageDemonTests/SDWebImageDemonTests.m
@@ -0,0 +1,39 @@
+//
+//  SDWebImageDemonTests.m
+//  SDWebImageDemonTests
+//
+//  Created by SevenCelsius on 16/5/29.
+//  Copyright © 2016年 SevenCelsius. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDWebImageDemonTests : XCTestCase
+
+@end
+
+@implementation SDWebImageDemonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
